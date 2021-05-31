96279779811d289e56c404cb6006db350255f6a4
Janusz Chorko <jchorko@gmail.com>
2021-05-31 12:39:06 +0200

    Update version

--- USPF_Menu.lua
+++ USPF_Menu.lua
@@ -2,7 +2,7 @@ if USPF == nil then USPF = {} end
 
 local ADDON_NAME = GetString(USPF_GUI_TITLE)
 local ADDON_AUTHOR = "Urich"
-local ADDON_VERSION = "5.7.0"
+local ADDON_VERSION = "5.7.1"
 
 -- Original 5.2.0 
 		-- Fonts = {

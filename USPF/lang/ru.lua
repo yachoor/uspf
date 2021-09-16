152f42bd0927243cadd2c9a253d9558fe092f748
Janusz Chorko <jchorko@gmail.com>
2021-09-16 12:05:00 +0200

    Add russian translation by K1nor

---
+++ USPF/lang/ru.lua
@@ -0,0 +1,123 @@
+SafeAddString(USPF_SETTINGS_FONT_TITLE, "Настройки шрифта")
+
+SafeAddString(USPF_SETTINGS_FONT_TITLE_HEADER, "Шрифт заголовков/футера")
+SafeAddString(USPF_SETTINGS_FONT_TITLE_DESC, "Настройте шрифт заголовков и футера в интерфейсе модификации.")
+
+SafeAddString(USPF_SETTINGS_FONT_GSP_TITLE, "Шрифт общих очков навыков")
+SafeAddString(USPF_SETTINGS_FONT_GSP_ROWS, "Настройте шрифт общих очков навыков в интерфейсе модификации.")
+
+SafeAddString(USPF_SETTINGS_FONT_SQS_TITLE, "Шрифт квестов и небесных осколков")
+SafeAddString(USPF_SETTINGS_FONT_SQS_ROWS, "Настройте шрифт квестов и небесных осколков в интерфейсе модификации.")
+
+SafeAddString(USPF_SETTINGS_FONT_GDQ_TITLE, "Шрифт квестов групповых подземелий")
+SafeAddString(USPF_SETTINGS_FONT_GDQ_ROWS, "Настройте шрифт квестов групповых подземелий в интерфейсе модификации.")
+
+SafeAddString(USPF_SETTINGS_FONT_PDB_TITLE, "Шрифт групповых событий откртых подземелий")
+SafeAddString(USPF_SETTINGS_FONT_PDB_ROWS, "Настройте шрифт групповых событий откртых подземелий в интерфейсе модификации.")
+
+SafeAddString(USPF_SETTINGS_RELOAD_WARNING, "Необходимо будет перезапустить интерфейс.")
+
+
+SafeAddString(USPF_SETTINGS_COLOR_TITLE, "Настройки цвета")
+SafeAddString(USPF_SETTINGS_COLOR_GSP_DONE, "Общие - Получено")
+SafeAddString(USPF_SETTINGS_COLOR_GSP_PROG, "Общие - В процессе")
+SafeAddString(USPF_SETTINGS_COLOR_GSP_NOT_DONE, "Общие - Не получено")
+SafeAddString(USPF_SETTINGS_COLOR_ZQ_DONE, "Квесты зон - Получено")
+SafeAddString(USPF_SETTINGS_COLOR_ZQ_PROG, "Квесты зон - В процессе")
+SafeAddString(USPF_SETTINGS_COLOR_ZQ_NOT_DONE, "Квесты зон - Не получено")
+SafeAddString(USPF_SETTINGS_COLOR_SS_DONE, "Небесные осколки - Получено")
+SafeAddString(USPF_SETTINGS_COLOR_SS_PROG, "Небесные осколки - В процессе")
+SafeAddString(USPF_SETTINGS_COLOR_SS_NOT_DONE, "Небесные осколки - Не получено")
+SafeAddString(USPF_SETTINGS_COLOR_GDQ_DONE, "Групповые подземелья - Получено")
+SafeAddString(USPF_SETTINGS_COLOR_GDQ_NOT_DONE, "Групповые подземелья - Не получено")
+SafeAddString(USPF_SETTINGS_COLOR_PDB_DONE, "Открытые подземелья - Получено")
+SafeAddString(USPF_SETTINGS_COLOR_PDB_NOT_DONE, "Открытые подземелья - Не получено")
+SafeAddString(USPF_SETTINGS_COLOR_DESC_DONE, "Настройте цвет для полученных очков навыков.")
+SafeAddString(USPF_SETTINGS_COLOR_DESC_PROG, "Настройте цвет для очков навыков, получение которых находится в процессе.")
+SafeAddString(USPF_SETTINGS_COLOR_DESC_NOT_DONE, "Настройте цвет для неполученных очков навыков.")
+
+
+SafeAddString(USPF_SETTINGS_SORT_TITLE, "Настройки сортировки")
+SafeAddString(USPF_SETTINGS_SORT_SQS, "Сортировка квестов и небесных осколков")
+SafeAddString(USPF_SETTINGS_SORT_SQS_DESC, "Настройте сортировку квестов и небесных осколков в интерфейсе модификации.")
+SafeAddString(USPF_SETTINGS_SORT_GDQ, "Сортировка групповых подземелий")
+SafeAddString(USPF_SETTINGS_SORT_GDQ_DESC, "Настройте сортировку квестов групповых подземелий в интерфейсе модификации.")
+SafeAddString(USPF_SETTINGS_SORT_PDB, "Сортировка групповых событий откртых подземелий")
+SafeAddString(USPF_SETTINGS_SORT_PDB_DESC, "Настройте сортировку групповых событий откртых подземелий в интерфейсе модификации.")
+
+
+SafeAddString(USPF_SETTINGS_OVERRIDE_TITLE, "Принудительное определение")
+SafeAddString(USPF_SETTINGS_OVERRIDE_FOLIUM, "Определение Folium Discognitum")
+SafeAddString(USPF_SETTINGS_OVERRIDE_FOLIUM_DESC, "Эта настройка позволит вам определить статус получения Folium Discognitum на вашем персонаже. Это пригодится вам, если модификация не отображает получение очков навыков от этой книги.")
+SafeAddString(USPF_SETTINGS_OVERRIDE_WARN, "Может стать причиной неправильного отображения общего количества очков навыков в интерфейсе USPF.")
+SafeAddString(USPF_SETTINGS_OVERRIDE_FOLIUM_SET, "Персонаж прочел Folium Discognitum")
+SafeAddString(USPF_SETTINGS_OVERRIDE_FOLIUM_SET_DESC, "Эта настройка позволит вам принудительно отметить прочтение Folium Discognitum. Для корректного отображения этого статуса необходимо включить опцию \"Принудительное определение\"")
+
+SafeAddString(USPF_SETTINGS_OVERRIDE_MWC_SET, "Определение обучения Морровинда")
+SafeAddString(USPF_SETTINGS_OVERRIDE_MWC_SET_DESC, "Эта настройка позволит вам определить статус прохождения обучающего квеста Морровинда. Она может вам пригодиться только в том случае, если ваш персонаж был создан после выхода главы Морровинд и пропустил обучающий квест \"Разрушенные оковы\".")
+
+SafeAddString(USPF_SETTINGS_OVERRIDE_SSC_SET, "Определение обучения Саммерсета")
+SafeAddString(USPF_SETTINGS_OVERRIDE_SSC_SET_DESC, "Эта настройка позволит вам определить статус прохождения обучающего квеста Саммерсета. Она может вам пригодиться только в том случае, если ваш персонаж был создан после выхода главы Саммерсет и пропустил обучающий квест \"Ловушка разума\".")
+
+SafeAddString(USPF_SETTINGS_OVERRIDE_EWC_SET, "Определение обучения Эльсвейра")
+SafeAddString(USPF_SETTINGS_OVERRIDE_EWC_SET_DESC, "Эта настройка позволит вам определить статус прохождения обучающего квеста Эльсвейра. Она может вам пригодиться только в том случае, если ваш персонаж был создан после выхода главы Эльсвейр и пропустил обучающий квест \"Яркие луны, теплые пески\".")
+
+
+SafeAddString(USPF_SETTINGS_OVERRIDE_GMC_SET, "Определение обучения Греймура")
+SafeAddString(USPF_SETTINGS_OVERRIDE_GMC_SET_DESC, "Эта настройка позволит вам определить статус прохождения обучающего квеста Греймура. Она может вам пригодиться только в том случае, если ваш персонаж был создан после выхода главы Греймур и пропустил обучающий квест \"Кровные узы\".")
+
+SafeAddString(USPF_SETTINGS_OVERRIDE_BWC_SET, "Определение обучения Черного Леса")
+SafeAddString(USPF_SETTINGS_OVERRIDE_BWC_SET_DESC, "Эта настройка позволит вам определить статус прохождения обучающего квеста Черного Леса. Она может вам пригодиться только в том случае, если ваш персонаж был создан после выхода главы Черный Лес и пропустил обучающий квест \"Адамантовые врата\".")
+
+
+SafeAddString(USPF_GUI_CHAR_LEVEL, "Уровень персонажа")
+SafeAddString(USPF_GUI_MAIN_QUEST, "Главная сюжетная линия")
+SafeAddString(USPF_GUI_FOLIUM, "Folium Discognitum")
+SafeAddString(USPF_GUI_MW_CHAR, "Обучение Морровинда")
+SafeAddString(USPF_GUI_SU_CHAR, "Обучение Саммерсета")
+SafeAddString(USPF_GUI_EW_CHAR, "Обучение Эльсвейра")
+SafeAddString(USPF_GUI_GM_CHAR, "Обучение Греймура")
+SafeAddString(USPF_GUI_BW_CHAR, "Обучение Черного Леса")
+SafeAddString(USPF_GUI_AVA_RANK, "Ранг на войне альянсов")
+SafeAddString(USPF_GUI_MAEL_ARENA, "Вихревая арена")
+
+
+SafeAddString(USPF_GUI_TITLE, "Urich's Skill Point Finder")
+
+SafeAddString(USPF_GUI_GSP, "Общие очки навыков")
+SafeAddString(USPF_GUI_SQS, "Сюжетные задания и небесные осколки")
+SafeAddString(USPF_GUI_GDQ, "Квесты групповых подземелий")
+SafeAddString(USPF_GUI_PDB, "Боссы групповых событий открытых подземелий")
+SafeAddString(USPF_GUI_SOURCE, "Источник")
+SafeAddString(USPF_GUI_PROGRESS, "Прогресс")
+SafeAddString(USPF_GUI_ZONE, "Область")
+SafeAddString(USPF_GUI_STORYLINE, "Сюжетная линия")
+SafeAddString(USPF_GUI_SKYSHARDS, "Небесные осколки")
+SafeAddString(USPF_GUI_GROUP_DUNGEON, "Групповое подземелье")
+SafeAddString(USPF_GUI_PUBLIC_DUNGEON, "Открытое подземелье")
+SafeAddString(USPF_GUI_DUNGEON_NAME, "Название подземелья")
+
+SafeAddString(USPF_GUI_TOTAL, "Получено")
+SafeAddString(USPF_GUI_CHAR_TOTAL, "Персонажем получено")
+
+
+SafeAddString(SI_BINDING_NAME_USPF_TOGGLE, "Показать/скрыть окно USPF.")
+
+SafeAddString(USPF_GUI_ZN_LCL, "Нижний")
+SafeAddString(USPF_GUI_ZN_UCL, "Верхний")
+SafeAddString(USPF_GUI_ZN_MQ, "Основная сюжетная линия")
+
+SafeAddString(USPF_MSG_SHOW_GUI, "USPF выключен.")
+SafeAddString(USPF_MSG_HIDE_GUI, "USPF скрыт.")
+
+SafeAddString(USPF_MSG_BAD_SLASH, "USPF неправильная команда.")
+SafeAddString(USPF_MSG_CMD_TITLE, "USPF консольные команды:")
+SafeAddString(USPF_MSG_CMD_OPTION, "    /uspf - Запуск окна модификации. Может быть назначена на горячую клавишу.")
+SafeAddString(USPF_MSG_ACTIVATE, "включить")
+SafeAddString(USPF_MSG_DEACTVATE, "выключить")
+
+SafeAddString(USPF_MSG_INIT, "Запуск USPF впервые!")
+SafeAddString(USPF_MSG_HELP, "USPF Включен!")
+
+SafeAddString(USPF_QUEST_NA, "Эти очки навыков даются не за задание.")
+SafeAddString(USPF_QUEST_NONE, "В этой зоне нет заданий, которые дают очки навыков.")
